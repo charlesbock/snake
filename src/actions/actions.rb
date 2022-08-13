ec148156620d2482f4eb49953023ce9bc66dd289
Charles Bock <charlesbock@gmail.com>
2022-08-12 23:32:33 -0500

    growing snake game over condition

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -3,7 +3,9 @@ module Actions
     next_direction = state.current_direction
     next_position = calc_next_position(state)
     
-    if position_is_valid?(state, next_position)
+    if position_is_food?(state, next_position)
+      grow_snake_to(state, next_position)
+    elsif position_is_valid?(state, next_position)
       move_snake_to(state, next_position)
     else
       end_game(state)
@@ -81,5 +83,15 @@ module Actions
     end
 
     return false
+  end
+
+  def self.grow_snake_to(state, next_position)
+    new_positions = [next_position] + state.snake.positions
+    state.snake.positions = new_positions
+    state
+  end
+
+  def self.position_is_food?(state, next_position)
+    state.food.row == next_position.row && state.food.col == next_position.col
   end
 end
